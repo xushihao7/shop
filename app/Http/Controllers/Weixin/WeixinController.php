68ae925b37d3e0d1df41ee2c0288c5b5c5b66e86
xushihao7 <1501024920.com>
2019-02-21 10:36:45 +0800

    修改

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -42,7 +42,10 @@ class WeixinController extends Controller
 
         //解析XML
         $xml = simplexml_load_string($data);  //将 xml字符串 转换成对象
-
+        //记录日志
+        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
+        file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
+        
         $event = $xml->Event;                 //事件类型
         //var_dump($xml);echo '<hr>';
         $openid = $xml->FromUserName;               //用户openid
