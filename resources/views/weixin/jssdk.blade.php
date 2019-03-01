02d0dc8c0e76b3d780328dadf95724b61bf73e37
xushihao7 <1501024920.com>
2019-03-01 15:06:40 +0800

    微信jssdk2

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -9,6 +9,7 @@
 @section("footer")
     @parent
     <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
+
     <script>
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
@@ -18,7 +19,9 @@
             signature: "{{$jsconfig['sign']}}",// 必填，签名
             jsApiList: ['chooseImage','uploadImage','getLocalImgData','startRecord'] // 必填，需要使用的JS接口列表
         });
+
         wx.ready(function(){
+
             $("#btn1").click(function () {
                 wx.chooseImage({
                     count: 9, // 默认9
@@ -29,7 +32,7 @@
                     }
                 });
             });
-        });
 
+        });
     </script>
 @endsection
